b255a0d50c4553ed1940428dc799bc413d235040
Niklas Nilsson <nilsson.sthlm@gmail.com>
2023-12-07 09:55:09 +0100

    fixes

--- IntervjuListaAvsnitt/API/APIClient.swift
+++ IntervjuListaAvsnitt/API/APIClient.swift
@@ -10,12 +10,14 @@ import Combine
 
 struct APIClient {
     
-    private let urlSession: URLSession = .shared
+    private static let urlSession: URLSession = .shared
     
     /*
         URLAR:
         Alla program: "https://api.sr.se/api/v2/programs?format=json&pagination=false"
         Ett program: "http://api.sr.se/api/v2/programs/(programId)?format=json"
+     
+        Implementera valfri method.
      */
     
     
@@ -28,40 +30,10 @@ struct APIClient {
         fatalError()
     }
     
-    static func getAllPrograms() async -> Result<[Program], Error> {
+    static func getAllPrograms() async throws -> [Program] {
         fatalError()
     }
-    
-//    static func getAllPrograms() -> AnyPublisher<Result<[Program], Error>, Never> {
-//        let programsResponse: AnyPublisher<Result<ProgramsResponse, Error>, Never> = Self.fireRequest(url: allProgramsURL)
-//        return programsResponse.map { result in
-//            return result.map(\.programs)
-//        }
-//        .eraseToAnyPublisher()
-//    }
-//    
-//    private static func fireRequest<Response: Decodable>(url: URL) -> AnyPublisher<Result<Response, Error>, Never> {
-//        let urlRequest: URLRequest = URLRequest(url: allProgramsURL)
-//        return URLSession.shared.dataTaskPublisher(for: urlRequest)
-//            .tryMap {
-//                if let httpResponse = $0.response as? HTTPURLResponse {
-//                    switch httpResponse.statusCode {
-//                    case 400...499:
-//                        throw APIClientError.clientNetworkError
-//                    case 500...:
-//                        throw APIClientError.serverError
-//                    default:
-//                        break
-//                    }
-//                }
-//                guard $0.data.count > 0 else { throw URLError(.zeroByteResource) }
-//                return $0.data
-//            }
-//            .decode(type: Response.self, decoder: JSONDecoder())
-//            .mapToResult()
-//            .eraseToAnyPublisher()
-//    }
-    
+
 }
 
 extension Publisher {
